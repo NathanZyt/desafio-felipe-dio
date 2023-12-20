bfce4398f695a70fbf46ec12e62b4c0830a6502d
NathanZyt <nathansantosgamer7@gmail.com>
2023-12-20 17:05:11 -0300

    adicionei algumas coisas novas

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("oe");
+let seuNome = "Nathan"
+let nivelDeXP = "1000"
+let titulo = ""
+
+if(nivelDeXP < 1000){
+    titulo = 'Ferro';
+}else if(nivelDeXP >= 1001 && nivelDeXP <= 2000){
+    titulo = 'Bronze';
+}else if(nivelDeXP >= 2001 && nivelDeXP <= 5000){
+    titulo = 'Prata'
+}else if(nivelDeXP >= 5001 && nivelDeXP <= 7000){
+    titulo = 'Ouro'
+}else if(nivelDeXP >= 7001 && nivelDeXP <= 8000){
+    titulo = 'Platina'
+}else if(nivelDeXP >= 8001 && nivelDeXP <= 9000){
+    titulo = 'Ascendente'
+}else if(nivelDeXP >= 9001 && nivelDeXP <= 10000){
+    titulo = 'Imortal'
+}else {
+    titulo = 'Radiante'
+}
+
+
+console.log("O herói cujo nome é " + seuNome + " está no nível de " + titulo)
